70f5a769deb81e9b3764976c6217acf899c53f70
Akshat Kedia <akshat.kedia@outlook.com>
2017-01-24 13:34:40 +0530

    concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,34 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const gutil = require('gulp-util');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
 
 const siteRoot = '_site';
 const cssFiles = 'assets/css/.?(s)css';
+const jsFiles = [
+                 'assets/js/jquery.min.js',
+                 'assets/js/bootstrap.min.js',
+                 'assets/js/flexslider.min.js',
+                 'assets/js/lightbox.min.js',
+                 'assets/js/masonry.min.js',
+                 'assets/js/ytplayer.min.js',
+                 'assets/js/smooth-scroll.min.js',
+                 'assets/js/parallax.js',
+                 'assets/js/scripts.js'
+                ];
 
 gulp.task('css', () => {
   gulp.src(cssFiles)
     .pipe(sass())
     .pipe(concat('all.css'))
     .pipe(gulp.dest('assets'));
+});
+
+gulp.task('js', () => {
+  gulp.src(jsFiles)
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('assets/js/'));
 });
 
 gulp.task('jekyll', () => {
@@ -42,7 +61,7 @@ gulp.task('serve', () => {
     }
   });
 
-  gulp.watch(cssFiles, ['css']);
+  gulp.watch(cssFiles, jsFiles, ['css', 'js']);
 });
 
-gulp.task('default', ['css', 'jekyll', 'serve']);
+gulp.task('default', ['css', 'js', 'jekyll', 'serve']);
