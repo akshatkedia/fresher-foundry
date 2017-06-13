054f9307f8bb25364d607459ed0274a18d867eae
Akshat Kedia <akshat.kedia@outlook.com>
2017-06-13 12:12:59 +0530

    updated opening layout and stylesheet folder organization

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,7 @@
-const child = require('child_process');
-const browserSync = require('browser-sync').create();
-
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const gutil = require('gulp-util');
-const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
-const siteRoot = '_site';
-const cssFiles = 'assets/css/.?(s)css';
 const jsFiles = [
                  'assets/js/jquery.min.js',
                  'assets/js/bootstrap.min.js',
@@ -21,13 +14,6 @@ const jsFiles = [
                  'assets/js/scripts.js'
                 ];
 
-gulp.task('css', () => {
-  gulp.src(cssFiles)
-    .pipe(sass())
-    .pipe(concat('all.css'))
-    .pipe(gulp.dest('assets'));
-});
-
 gulp.task('js', () => {
   gulp.src(jsFiles)
       .pipe(concat('all.js'))
@@ -35,33 +21,8 @@ gulp.task('js', () => {
       .pipe(gulp.dest('assets/js/'));
 });
 
-gulp.task('jekyll', () => {
-  const jekyll = child.spawn('jekyll', ['build',
-    '--watch',
-    '--incremental',
-    '--drafts'
-  ]);
-
-  const jekyllLogger = (buffer) => {
-    buffer.toString()
-      .split(/\n/)
-      .forEach((message) => gutil.log('Jekyll: ' + message));
-  };
-
-  jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
-});
-
 gulp.task('serve', () => {
-  browserSync.init({
-    files: [siteRoot + '/**'],
-    port: 4000,
-    server: {
-      baseDir: siteRoot
-    }
-  });
-
-  gulp.watch(cssFiles, jsFiles, ['css', 'js']);
+  gulp.watch(jsFiles, ['js']);
 });
 
-gulp.task('default', ['css', 'js', 'jekyll', 'serve']);
+gulp.task('default', ['js', 'serve']);
